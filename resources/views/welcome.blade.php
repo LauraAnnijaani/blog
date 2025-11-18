2537f40cdbcad9090607e352421334078c2db0b5
Laura Annijaani <laura.annijaani@tptlive.ee>
2025-11-18 15:30:29 +0200

    Add multiple images

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,21 @@
     <div class="grid grid-cols-4 gap-2">
         @foreach ($posts as $post)
             <div class="card bg-base-300 shadow-sm">
-                @if($post->image)
+
+                @if ($post->images->count() === 1)
                     <figure>
-                        <img src="{{ $post->image->path }}" />
+                        <img src="{{ $post->images->first()->path }}" />
                     </figure>
+                @elseif($post->images->count() > 1)
+                    <div class="carousel rounded-box">
+                        @foreach ($post->images as $image)
+                            <div class="carousel-item w-full">
+                                <img src="{{ $image->path }}" />
+                            </div>
+                        @endforeach
+                    </div>
                 @endif
+
                 <div class="card-body">
                     <h2 class="card-title">{{ $post->title }}</h2>
                     <p>{{ $post->snippet }}</p>
