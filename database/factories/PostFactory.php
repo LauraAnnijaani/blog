7611be10426e00454dd4c8445c3cc473cc0fabd6
Laura Annijaani <laura.annijaani@tptlive.ee>
2025-11-18 14:35:07 +0200

    Add single image example

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -18,8 +18,12 @@ class PostFactory extends Factory
     {
         $created = fake()->dateTimeBetween();
         $updated = fake()->dateTimeBetween($created);
-        if(rand(0, 9)){
+        if (rand(0, 9)) {
             $updated = $created;
+        }
+        $image = null;
+        if (rand(0, 9)) {
+            $image = 'https://picsum.photos/seed/' . fake()->unique()->uuid . '/640/480';
         }
 
         return [
@@ -27,6 +31,7 @@ class PostFactory extends Factory
             'body' => fake()->paragraphs(6, true),
             'created_at' => $created,
             'updated_at' => $updated,
+            'image' => $image,
         ];
     }
 }
