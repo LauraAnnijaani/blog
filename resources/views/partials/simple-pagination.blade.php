5f83cffa5bb6772932cfd6a676b4aa91c1b76f67
Laura Annijaani <laura.annijaani@tptlive.ee>
2025-11-24 10:39:09 +0200

    Add daisyui pagination

--- resources/views/partials/simple-pagination.blade.php
+++ resources/views/partials/simple-pagination.blade.php
@@ -1,18 +1,32 @@
 @if ($paginator->hasPages())
     <nav>
-        <ul class="pagination">
+        <ul class="join mb-3">
             {{-- Previous Page Link --}}
             @if ($paginator->onFirstPage())
-                <li class="disabled" aria-disabled="true"><span>@lang('pagination.previous')</span></li>
+                <li class="join-item">
+                    <a href="{{ $paginator->previousPageUrl() }}" rel="prev">
+                        <button class="join-item btn btn-outline">Previous</button>
+                    </a>
+                </li>
             @else
-                <li><a href="{{ $paginator->previousPageUrl() }}" rel="prev">@lang('pagination.previous')</a></li>
+                <li class="join-item">
+                    <a href="{{ $paginator->previousPageUrl() }}" rel="prev">
+                        <button class="join-item btn btn-outline">Previous</button>
+                    </a>
+                </li>
             @endif
 
             {{-- Next Page Link --}}
             @if ($paginator->hasMorePages())
-                <li><a href="{{ $paginator->nextPageUrl() }}" rel="next">@lang('pagination.next')</a></li>
+                <li class="join-item">
+                    <a href="{{ $paginator->nextPageUrl() }}" rel="next">
+                        <button class="join-item btn btn-outline">Next</button>
+                    </a>
+                </li>
             @else
-                <li class="disabled" aria-disabled="true"><span>@lang('pagination.next')</span></li>
+                <li class="join-item">
+                    <button class="btn btn-outline" disabled>Next</button>
+                </li>
             @endif
         </ul>
     </nav>
