c72e4ba3bd9a59acb585db24a2ba750624ca4ff7
Laura Annijaani <laura.annijaani@tptlive.ee>
2025-11-26 15:28:36 +0200

    Forgot and reset password pages daisyui

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,35 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+@extends('partials.layout')
+@section('title', 'Dashboard page')
+@section('content')
+    <div class="card bg-base-300">
+        <div class="card-body">
+            @if (session('status'))
+                <div class="alert alert-success mb-4">
+                    {{ session('status') }}
+                </div>
+            @endif
+            <form method="POST" action="{{ route('password.email') }}">
+                @csrf
+                <div class="mt-2 mb-4">
+                    {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+                </div>
+
+                <fieldset class="fieldset">
+                    <legend class="fieldset-legend">{{ __('Email') }}</legend>
+                    <input value="{{ old('email') }}" name="email" type="email" required autofocus
+                        autocomplete="username" class="input w-full @error('email') input-error @enderror"
+                        placeholder="Email" />
+                    @error('email')
+                        <p class="label text-error">{{ $message }}</p>
+                    @enderror
+                </fieldset>
+
+                <div class="flex justify-end mt-4">
+                    <button class="btn btn-primary">
+                        {{ __('Email Password Reset Link') }}
+                    </button>
+                </div>
+            </form>
+        </div>
     </div>
-
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+@endsection
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,39 +1,52 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('password.store') }}">
-        @csrf
+@extends('partials.layout')
+@section('title', 'Dashboard page')
+@section('content')
+    <div class="card bg-base-300">
+        <div class="card-body">
+            <form method="POST" action="{{ route('password.store') }}">
+                @csrf
 
-        <!-- Password Reset Token -->
-        <input type="hidden" name="token" value="{{ $request->route('token') }}">
+                <!-- Password Reset Token -->
+                <input type="hidden" name="token" value="{{ $request->route('token') }}">
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                <!-- Email Address -->
+                <fieldset class="fieldset">
+                    <legend class="fieldset-legend">{{ __('Email') }}</legend>
+                    <input value="{{ old('email', $request->email) }}" name="email" type="email" required
+                        autocomplete="username" class="input w-full @error('email') input-error @enderror"
+                        placeholder="Email" readonly />
+                    @error('email')
+                        <p class="label text-error">{{ $message }}</p>
+                    @enderror
+                </fieldset>
+
+                <!-- New Password -->
+                <fieldset class="fieldset">
+                    <legend class="fieldset-legend">{{ __('New Password') }}</legend>
+                    <input name="password" type="password" required autocomplete="new-password"
+                        class="input w-full @error('password') input-error @enderror" placeholder="New Password" />
+                    @error('password')
+                        <p class="label text-error">{{ $message }}</p>
+                    @enderror
+                </fieldset>
+
+                <!-- Confirm Password -->
+                <fieldset class="fieldset">
+                    <legend class="fieldset-legend">{{ __('Confirm Password') }}</legend>
+                    <input name="password_confirmation" type="password" required autocomplete="new-password"
+                        class="input w-full @error('password_confirmation') input-error @enderror"
+                        placeholder="Confirm Password" />
+                    @error('password_confirmation')
+                        <p class="label text-error">{{ $message }}</p>
+                    @enderror
+                </fieldset>
+
+                <div class="flex justify-end mt-4">
+                    <button class="btn btn-primary">
+                        {{ __('Reset Password') }}
+                    </button>
+                </div>
+            </form>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Reset Password') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </div>
+@endsection
